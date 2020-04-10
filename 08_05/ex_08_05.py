ee775444775c58fe5f8f8771fc94069093d0b90f
Dmytro M <dmytro.malyk@gmail.com>
2020-04-10 00:21:06 -0500

    Assignment 8.5

---
+++ 08_05/ex_08_05.py
@@ -0,0 +1,13 @@
+# Assignment 8.5
+fname = input("Enter file name: ")
+#if len(fname) < 1 : fname = "mbox-short.txt"
+fh = open(fname)
+count = 0
+for line in fh:
+    line = line.rstrip()
+    if not line.startswith('From '): continue
+    femail = line.split()
+    print(femail[1])
+    count = count +1
+print("There were", count, "lines in the file with From as the first word")
+# The End
